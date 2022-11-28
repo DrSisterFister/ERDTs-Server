8b7cec329b44fe0bee8a6a8f4536ecbb6718915f
David <dav.gruber@htlstp.at>
2022-11-28 23:20:36 +0100

    add: aes-decryption

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const { SerialPort, ByteLengthParser} =require('serialport');
+const {SerialPort, ByteLengthParser} = require('serialport')
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc'
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 
-const port = new SerialPort({
-    path: '/dev/serial0',
-    baudRate: 9600
-})
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv)
+    let encrypted = cipher.update(text)
+    encrypted = Buffer.concat([encrypted, cipher.final()])
+    return {iv: iv.toString('hex'), encryptedData: encrypted.toString('hex')}
+}
 
-const parser = port.pipe(new ByteLengthParser({length: 8}))
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex')
+    let encryptedText = Buffer.from(text.encryptedData, 'hex')
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv)
+    let decrypted = decipher.update(encryptedText)
+    decrypted = Buffer.concat([decrypted, decipher.final()])
+    return decrypted.toString()
+}
 
-parser.on('data', console.log)
+//const port = new SerialPort({
+//    path: '/dev/serial0',
+//    baudRate: 9600
+//})
+//
+//const parser = port.pipe(new ByteLengthParser({length: 8}))
+//
+//parser.on('data', console.log)
+
+
+var hw = encrypt("Some serious stuff")
+console.log(hw)
+console.log(decrypt(hw))
