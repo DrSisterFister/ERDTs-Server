8af04ad97bf3a0d12796c37bad105e0baddffb0f
Doc <43089687+DrSisterFister@users.noreply.github.com>
2022-11-30 10:50:40 +0100

    add: Loginpage

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loginform.css"
 
 const LoginForm = () => {
+
+    const [popupStyle, showPopup] = useState("hide")
+
+    const popup = () => {
+        showPopup("login-popup");
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+
     return(
-        <div>
+        <div className="cover">
+            <h1>LOGIN</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+
+            <div className="login-btn" onClick={popup}>Login</div>
+
+            <p className="text">Or login using</p>
+
+            <div className={popupStyle}>
+            <h3>Login Failed</h3>
+            <p>Username or password incorrect</p>
+            </div>
 
         </div>
     )
